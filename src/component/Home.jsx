236921902db33d376130ae35714cd36d88502b63
Gopi <mr.gopi0004@gmail.com>
2026-02-12 21:41:12 +0530

    feat: added featurss

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <section className="section home">
       <h1>Hi, I'm Gopi</h1>
-      <h3>Java Backend / Full Stack Developer</h3>
+      <h3>Java Full Stack Developer</h3>
       <p>
         I build simple and scalable web applications and I am looking for a
         fresher opportunity in IT.
